b7609daff90b29e06a6c94417e747038e5f080ea
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-08-16 17:08:38 +0300

    v23.01.21: run version source regression test

--- HattrickAI_Render_Web_v1/webapp/FormationTests/Program.cs
+++ HattrickAI_Render_Web_v1/webapp/FormationTests/Program.cs
@@ -3,4 +3,5 @@ using HattrickAI.FormationTests;
 var failures = 0;
 failures += HistoricalFormationMapperTests.RunAll();
 failures += SimulationCalibrationTests.RunAll();
+failures += VersionSourceTests.RunAll();
 return failures;
