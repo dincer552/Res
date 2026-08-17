d438d51cd9f78929d7ebf42aea3bf99c62661729
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-08-17 18:52:17 +0300

    Run individual order optimizer smoke test

--- HattrickAI_Render_Web_v1/webapp/FormationTests/Program.cs
+++ HattrickAI_Render_Web_v1/webapp/FormationTests/Program.cs
@@ -4,4 +4,5 @@ var failures = 0;
 failures += HistoricalFormationMapperTests.RunAll();
 failures += SimulationCalibrationTests.RunAll();
 failures += VersionSourceTests.RunAll();
+failures += IndividualOrderOptimizationTests.RunAll();
 return failures;
