391fb29768f7195cacee80dd54abaee5d5b62e6b
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-01 17:10:43 +0300

    test: add offline regression entrypoint

---
+++ HattrickAI_V5.OfflineTests/Program.cs
@@ -0,0 +1,4 @@
+using HattrickAI.V5.Core;
+
+var path = args.Length > 0 ? args[0] : "HattrickAI_V5_CHPP_FullOffline_2026-09-01T08-49-54-690Z(5).json";
+return await OfflineRegressionRunner.RunAsync(path);
