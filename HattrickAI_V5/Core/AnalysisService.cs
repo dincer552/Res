78db0ab8e95a6119bbde8a22ec2da9b8a9459547
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-03 21:08:04 +0300

    Parse CHPP set-piece skill into player profiles

--- HattrickAI_V5/Core/AnalysisService.cs
+++ HattrickAI_V5/Core/AnalysisService.cs
@@ -175,7 +175,7 @@ public sealed class AnalysisService
     {
         var xml = await _chpp.GetXmlAsync("players", new Dictionary<string, string?> { ["version"] = "1.3", ["teamId"] = teamId.ToString(CultureInfo.InvariantCulture) }, ct);
         return XmlV5.Root(xml)?.Descendants("Player").Select(p => new Player(
-            XmlV5.Int(p, "PlayerID"), XmlV5.Text(p, "PlayerName"), XmlV5.Int(p, "KeeperSkill"), XmlV5.Int(p, "DefenderSkill"), XmlV5.Int(p, "PlaymakerSkill"), XmlV5.Int(p, "PassingSkill"), XmlV5.Int(p, "WingerSkill"), XmlV5.Int(p, "ScorerSkill"), XmlV5.Int(p, "StaminaSkill"), XmlV5.Int(p, "PlayerForm"), XmlV5.Int(p, "Experience"), XmlV5.Int(p, "Loyalty"), XmlV5.Int(p, "InjuryLevel"), ParseSpecialty(p)))
+            XmlV5.Int(p, "PlayerID"), XmlV5.Text(p, "PlayerName"), XmlV5.Int(p, "KeeperSkill"), XmlV5.Int(p, "DefenderSkill"), XmlV5.Int(p, "PlaymakerSkill"), XmlV5.Int(p, "PassingSkill"), XmlV5.Int(p, "WingerSkill"), XmlV5.Int(p, "ScorerSkill"), XmlV5.Int(p, "StaminaSkill"), XmlV5.Int(p, "PlayerForm"), XmlV5.Int(p, "Experience"), XmlV5.Int(p, "Loyalty"), XmlV5.Int(p, "InjuryLevel"), ParseSpecialty(p), XmlV5.Int(p, "SetPiecesSkill")))
             .Where(p => p.Id > 0).ToList() ?? new();
     }
 
