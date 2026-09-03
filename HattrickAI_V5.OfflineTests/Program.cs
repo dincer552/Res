a66d03053f9e56f631b5be4e06521dc672afc69f
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-03 19:05:19 +0300

    V5 offline regression pipelineina FAZ I LS gate ekle

--- HattrickAI_V5.OfflineTests/Program.cs
+++ HattrickAI_V5.OfflineTests/Program.cs
@@ -1,4 +1,8 @@
 using HattrickAI.V5.Core;
+using HattrickAI.V5.OfflineTests;
+
+var longShotRegression = LongShotOpportunityRegression.Run();
+if (longShotRegression != 0) return longShotRegression;
 
 var path = args.Length > 0 ? args[0] : "HattrickAI_V5_CHPP_FullOffline_2026-09-01T08-49-54-690Z(5).json";
 return await FullPipelineRegressionRunner.RunAsync(path);
