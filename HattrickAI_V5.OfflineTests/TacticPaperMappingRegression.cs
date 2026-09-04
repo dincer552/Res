acad1bc62e909351074b2f41aac37f02afa5be8a
dincer552 <129761693+dincer552@users.noreply.github.com>
2026-09-04 06:49:18 +0300

    Finalize tactic-level to paper RT regression

--- HattrickAI_V5.OfflineTests/TacticPaperMappingRegression.cs
+++ HattrickAI_V5.OfflineTests/TacticPaperMappingRegression.cs
@@ -7,13 +7,27 @@ public static class TacticPaperMappingRegression
     public static int Run()
     {
         AssertNear(0.0, TacticPaperMappingEngine.ToPaperRt(0), "V5 0 -> RT 0");
-        AssertNear(20.0, TacticPaperMappingEngine.ToPaperRt(5), "V5 5 -> RT 20");
-        AssertNear(40.0, TacticPaperMappingEngine.ToPaperRt(10), "V5 10 -> RT 40");
+        AssertNear(10.0, TacticPaperMappingEngine.ToPaperRt(5), "V5 5 -> RT 10");
+        AssertNear(20.0, TacticPaperMappingEngine.ToPaperRt(10), "V5 10 -> RT 20");
+        AssertNear(20.0, TacticPaperMappingEngine.ToPaperRt(999), "V5 upper clamp -> RT 20");
         AssertNear(0.0, TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.Normal, 5), "Normal has no TCR");
 
-        AssertNear(M8ChanceAllocationEngine.CalculateTacticConversionRate(AdvancedTactic.AttackMiddle, 20), TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.AttackMiddle, 5), "V5 5 -> paper RT 20 AiM");
-        AssertNear(M8ChanceAllocationEngine.CalculateTacticConversionRate(AdvancedTactic.AttackWings, 20), TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.AttackWings, 5), "V5 5 -> paper RT 20 AoW");
-        AssertNear(M8ChanceAllocationEngine.CalculateTacticConversionRate(AdvancedTactic.LongShots, 20), TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.LongShots, 5), "V5 5 -> paper RT 20 LS");
+        AssertNear(
+            M8ChanceAllocationEngine.CalculateTacticConversionRateFromPaperRt(AdvancedTactic.AttackMiddle, 20),
+            TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.AttackMiddle, 10),
+            "V5 10 -> paper RT 20 AiM");
+        AssertNear(
+            M8ChanceAllocationEngine.CalculateTacticConversionRateFromPaperRt(AdvancedTactic.AttackWings, 20),
+            TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.AttackWings, 10),
+            "V5 10 -> paper RT 20 AoW");
+        AssertNear(
+            M8ChanceAllocationEngine.CalculateTacticConversionRateFromPaperRt(AdvancedTactic.LongShots, 20),
+            TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.LongShots, 10),
+            "V5 10 -> paper RT 20 LS");
+        AssertNear(
+            M8ChanceAllocationEngine.CalculateTacticConversionRateFromPaperRt(AdvancedTactic.CounterAttack, 20),
+            TacticPaperMappingEngine.PaperTacticConversionRate(AdvancedTactic.CounterAttack, 10),
+            "V5 10 -> paper RT 20 CA");
 
         Console.WriteLine("TacticPaperMappingRegression: PASS");
         return 0;
